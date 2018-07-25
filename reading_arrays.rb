cdafc22723961e13d98234ec9dba3633aac9e7a4
Ella Jaravata <ella.jaravata@gmail.com>
2018-07-25 14:16:39 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,7 +1,7 @@
-STUDENT_NAMES = [ "Adele","Beyoncé","Cardi B", "Lady Gaga","Nicki Minaj", "Rihanna"]
+STUDENT_NAMES = [ "Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
 
 def first_student_by_index
-  student_names[1]
+  student_names[0]
 end
 
 def fourth_student_by_index
